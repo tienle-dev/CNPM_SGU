e9b91dbe0d6503a7b2014c01c08b2b9ec553a77b
tienle-dev <lmthaha1414@gmail.com>
2025-10-19 22:49:10 +0700

    update changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://utkarshanand:utkarshtomatoanand@cluster0.gr6lr.mongodb.net/food-del').then(()=>console.log("DB Connected"))
+    await mongoose.connect('mongodb+srv://minhtien123:minhtien123@foodfastdelivery.wz7wtg4.mongodb.net/').then(()=>console.log("DB Connected"))
 }
 
