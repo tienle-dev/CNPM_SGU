9d8f3aac483ea79fd9ef0a35e167582da1f79ac8
tinhnguyen0123 <tinhnguyen0123@gmail.com>
2025-10-25 19:09:17 +0700

    reconnect

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://huutinhnguyen1502:tinh1502@foodfastdelivery.wz7wtg4.mongodb.net/FoodFast').then(()=>console.log("DB Connected"))
+    await mongoose.connect('mongodb+srv://minhtien123:minhtien123@foodfastdelivery.wz7wtg4.mongodb.net/FoodFast').then(()=>console.log("DB Connected"))
 }
 
